31b7b333b8f9e102a4efa8ff5da09b42906b3998
Donavie Ordonez <donavielove@csu.fullerton.edu>
2021-05-16 22:45:46 -0700

    changes

--- chat.php
+++ chat.php
@@ -6,83 +6,90 @@ date_default_timezone_set('UTC');
 //connect to database
 $db = mysqli_connect('mariadb', 'cs431s31', 'aoma2Gei', 'cs431s31');
 if (mysqli_connect_errno()) {
-   echo '<p>Error: Could not connect to database.<br/>
+    echo '<p>Error: Could not connect to database.<br/>
    Please try again later.</p>';
-   exit;
+    exit;
 }
 //helper funtion to replace get_results() if without mysqlnd 
-function get_result( $Statement ) {
+function get_result($Statement)
+{
     $RESULT = array();
     $Statement->store_result();
-    for ( $i = 0; $i < $Statement->num_rows; $i++ ) {
+    for ($i = 0; $i < $Statement->num_rows; $i++) {
         $Metadata = $Statement->result_metadata();
         $PARAMS = array();
-        while ( $Field = $Metadata->fetch_field() ) {
-            $PARAMS[] = &$RESULT[ $i ][ $Field->name ];
+        while ($Field = $Metadata->fetch_field()) {
+            $PARAMS[] = &$RESULT[$i][$Field->name];
         }
-        call_user_func_array( array( $Statement, 'bind_result' ), $PARAMS );
+        call_user_func_array(array($Statement, 'bind_result'), $PARAMS);
         $Statement->fetch();
     }
     return $RESULT;
 }
-try { 
+try {
     $currentTime = time();
-    $session_id = session_id();    
-    $lastPoll = isset($_SESSION['last_poll']) ? $_SESSION['last_poll'] : $currentTime;    
+    $session_id = session_id();
+    $lastPoll = isset($_SESSION['last_poll']) ? $_SESSION['last_poll'] : $currentTime;
     $action = isset($_SERVER['REQUEST_METHOD']) && ($_SERVER['REQUEST_METHOD'] == 'POST') ? 'send' : 'poll';
-    switch($action) {
+    switch ($action) {
         case 'poll':
-           $query = "SELECT * FROM chat WHERE date_created >= ".$lastPoll;
-           $stmt = $db->prepare($query);
-           $stmt->execute();
-           $stmt->bind_result($id, $message, $sent_by, $date_created, $username, $user_color);
-           $result = get_result( $stmt);
-           $newChats = [];
-           while($chat = array_shift($result)) {
-               
-               if($session_id == $chat['sent_by']) 
-                  $chat['username'] = 'self';
-               
-               $newChats[] = $chat;
+            $query = "SELECT * FROM chat WHERE date_created >= " . $lastPoll;
+            $stmt = $db->prepare($query);
+            $stmt->execute();
+            $stmt->bind_result($id, $message, $sent_by, $date_created, $username, $user_color);
+            $result = get_result($stmt);
+            $newChats = [];
+            while ($chat = array_shift($result)) {
+
+                if ($session_id == $chat['sent_by'])
+                    $chat['username'] = 'self';
+
+                $newChats[] = $chat;
             }
-           $_SESSION['last_poll'] = $currentTime;
+            $_SESSION['last_poll'] = $currentTime;
 
-           print json_encode([
+            print json_encode([
                 'success' => true,
-		'messages' => $newChats
-           ]);
-           exit;
-        case 'send':
+                'messages' => $newChats
+            ]);
+            exit;
 
-            //get the username, message and color and store into the database
-            $message = isset($_POST['message']) ? $_POST['message'] : '';            
+        case 'send':
+            $message = isset($_POST['message']) ? $_POST['message'] : '';
             $message = strip_tags($message);
             $user_name = isset($_POST['username']) ? $_POST['username'] : '';
-            $bubbleColor = '#'.substr(str_shuffle('ABCDEF0123456789'), 0, 6); 
 
-            // Adding color to user
-            $colorQuery = "SELECT username, user_color FROM chat";
-            $colorResult = $db->query($colorQuery);
-            while($pair = $colorResult->fetch_assoc()){
-
-              if($user_name == $pair["username"]){
-                $bubbleColor = $pair["user_color"];
-              }
-
+            // Creating random colors for chat bubbles
+            $randomNum = rand(1, 5);
+            switch ($randomNum) {
+                case 1:
+                    $bubbleColor = '#FF0000'; // red 
+                    break;
+                case 2:
+                    $bubbleColor = '#FF00FF'; // magenta
+                    break;
+                case 3:
+                    $bubbleColor = '#0000FF'; // blue
+                    break;
+                case 4:
+                    $bubbleColor = '#00FFFF'; // cyan
+                    break;
+                case 5:
+                    $bubbleColor = '#00FF00'; // lime
+                    break;
+                default:
             }
 
-            //modify query to add username and color 
-            $query = "INSERT INTO chat (message, sent_by, date_created, username, user_color) VALUES(?,?,?,?,?)";
+            $query = "INSERT INTO chat (message, sent_by, date_created, username, user_color) VALUES(?, ?, ?, ?, ?)";
             $stmt = $db->prepare($query);
-            $stmt->bind_param('ssi', $message, $session_id, $currentTime, $user_name, $bubbleColor); 
-            $stmt->execute(); 
+            $stmt->bind_param('sssis', $message, $session_id, $currentTime, $user_name, $bubbleColor);
+            $stmt->execute();
             print json_encode(['success' => true]);
             exit;
     }
-} catch(Exception $e) {
+} catch (Exception $e) {
     print json_encode([
         'success' => false,
         'error' => $e->getMessage()
     ]);
 }
-?>
--- client.js
+++ client.js
@@ -1,17 +1,17 @@
-var pollServer = function () {
-    $.get('chat.php', function (result) {
+var pollServer = function() {
 
-        if (!result.success) {
+    $.get('chat.php', function(result) {
+        
+        if(!result.success) {
             console.log("Error polling server for new messages!");
             return;
         }
-        // Generating colors for chat bubbles 
-        $.each(result.messages, function (idx) {
-
+        
+        $.each(result.messages, function(idx) {
+            
             var chatBubble;
-
             if(this.username == 'self') {
-                chatBubble = $('<div class="row bubble-sent pull-right" style="background:' + this.user_color+ '">' + 
+                chatBubble = $('<div class="row bubble-sent pull-right" style="background:' + this.user_color + '">' + 
                                'Me: ' + this.message + 
                                '</div><div class="clearfix"></div>');
             } else {
@@ -19,42 +19,44 @@ var pollServer = function () {
                                this.username + ': ' + this.message + 
                                '</div><div class="clearfix"></div>');
             }
-
+            
             $('#chatPanel').append(chatBubble);
         });
-
+        
         setTimeout(pollServer, 5000);
     });
 }
 
-$(document).on('ready', function () {
+$(document).on('ready', function() {
     pollServer();
 
-    $('button').click(function () {
+    $('button').click(function() {
         $(this).toggleClass('active');
     });
 });
 
-$('#sendMessageBtn').on('click', function (event) {
+$('#sendMessageBtn').on('click', function(event) {
+
     event.preventDefault();
 
+    //Checks the given nickname and message for null strings or sneaky sql injections.
     var username = $('#newname').val();
     var message = $('#chatMessage').val();
-
+    
+    //Sends the message and name to the server.
     $.post('chat.php', {
-        'message': message,
-        'username': username
-    }, function (result) {
-
+        'message' : message,
+        'username' : username,
+    }, function(result) {
+        
         $('#sendMessageBtn').toggleClass('active');
-
-        if (!result.success) {
+        
+        if(!result.success) {
             alert("There was an error sending your message");
         } else {
             console.log("Message sent!");
             $('#chatMessage').val('');
         }
     });
-
+    
 });
-
